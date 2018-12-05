64fde3a7ed8af4ca12008ad208e5a7dbc9ebff51
zbwbb <zhoubinxinna@sina.com>
2018-12-05 15:36:35 +0800

    32

--- Classes/ZCPKit.h
+++ Classes/ZCPKit.h
@@ -8,12 +8,5 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for ZCPKit.
-FOUNDATION_EXPORT double ZCPKitVersionNumber;
-
-//! Project version string for ZCPKit.
-FOUNDATION_EXPORT const unsigned char ZCPKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ZCPKit/PublicHeader.h>
 
 #import <ZCPKit/ZCPUtil.h>
