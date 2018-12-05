a4a11caf0e34b326cfe449acc5e7a2c6303be52b
zbwbb <zhoubinxinna@sina.com>
2018-12-05 16:30:16 +0800

    fix all

---
+++ WBBKit/Pods/Target Support Files/Pods-WBBKit/Pods-WBBKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WBBKit : NSObject
+@end
+@implementation PodsDummy_Pods_WBBKit
+@end
---
+++ WBBKit/WBBKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBBKit
+//
+//  Created by TsouMac2016 on 2018/12/5.
+//  Copyright © 2018 TsouMac2016. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
