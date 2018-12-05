85a37e52c0c869e140439f03df02d860151dc4db
zbwbb <zhoubinxinna@sina.com>
2018-12-05 15:33:18 +0800

    33333

--- Classes/ZCPKit.h
+++ Classes/ZCPKit.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ZCPKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZCPKit/PublicHeader.h>
 
-
+#import <ZCPKit/ZCPUtil.h>
